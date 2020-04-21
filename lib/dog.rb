48de8715bbd74c65fb0c0dda83801f17c4c88290
thealekid <chukorakwusi@gmail.com>
2020-04-21 14:50:12 +0100

    Updated

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,25 +1,20 @@
 class Dog
- def initialize 
-    @name = name
-    @breed = breed
- end
 
-def name=(name)
-    @name = name
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+
 end
-
-def name
-    @name
-end
-
-def breed=(breed)
-    @breed = breed
-end 
-
- def breed
-      @breed
-end 
-
-end 
-
 
--- lib/person.rb
+++ lib/person.rb
@@ -1,23 +1,22 @@
 class Person
-    def initialize 
-       @name = name
-       @job = job
+
+    def name=(name)
+        @name = name
     end
-   
-   def name=(name)
-       @name = name
-   end
-   
-   def name
-       @name
-   end
-   
-   def job=(job)
-       @job = job
-   end 
-   
+
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
     def job
-         @job
-   end 
-   
-   end 
+        @job
+    end
+
+
+end
+
+
